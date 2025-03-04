4419c69c2db2dc0f6ab2d33ba17d693ce4443402
Carlos Miguel Marquez Suela <126050848+carlossuela576@users.noreply.github.com>
2025-03-04 21:08:12 +0700

    Dynamic IO
    - integer & double input and outputs
    - narrowcasting
    - relocation of functions to "function.java"

--- Main.java
+++ Main.java
@@ -1,64 +1,28 @@
-import Operation.operations;
+import Operation.function;
 import java.util.Scanner;
 
 public class Main{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
-        do { 
-            //starting prompt & operation choice input
-            System.out.println();
-            startingPrompts();
-            String operationChoice = sc.nextLine().toLowerCase();
+        function fn1 = new function();
 
-            //two number input
-            operationPrompt();
-            System.out.print("Enter first number: ");
-            double x = sc.nextDouble();
-            System.out.print("Enter second number: ");
-            double y = sc.nextDouble();
-            sc.nextLine();
-            System.out.println();
+        //choose between integer an integer and double
+        System.out.println("Choose a data type: \nInteger \nDouble");
+        String formIn = sc.nextLine().toLowerCase();
+        System.out.println();
 
-            //call operation methods
-            switch (operationChoice) {
-                case "add":
-                    System.out.println(operations.add(x, y));
-                    break;
-                case "subtract":
-                    System.out.println(operations.subtract(x, y));
-                    break;
-                case "multiply":
-                    System.out.println(operations.multiply(x, y));
-                    break;
-                case "divide":
-                    System.out.println(operations.divide(x, y));
-                    break;
-                default:
-                    try {
-                        System.out.println("Your first number is " + x + " and your second number is " + y);
-                        System.out.println("Make sure to enter the correct inputs");
-                    } catch (Exception e) {
-                        // TODO: handle exception
-                        throw new ArithmeticException("Ensure correct inputs");
-                    }
-                    finally{
-                        System.out.println("try catch executed");
-                    }
-            }
-        } while (true);
-
-    }
-
-    //minor static methods
-    static void startingPrompts(){
-        System.out.println("Welcome to calculator");
-        System.out.println("Please select an operation");
-        System.out.println("----------------------------");
-        System.out.println("Add \nSubtract \nMultiply \nDivide");
-    }
-
-    static void operationPrompt(){
-        System.out.println("Please enter two numbers");
+        switch (formIn) {
+            case "integer":
+                double defaultOut = fn1.functions();
+                //narrowcasting: double --> integer
+                int changeOut = (int) defaultOut;
+                System.out.println(changeOut);
+                break;
+            case "double":
+                System.out.println(fn1.functions());
+                break;
+            default:
+                throw new AssertionError();
+        }
     }
 }
---
+++ Operation/function.java
@@ -0,0 +1,66 @@
+package Operation;
+import java.util.Scanner;
+
+public class function {
+    Scanner sc = new Scanner(System.in);
+    public double functions(){
+        startingPrompts();
+        String operationChoice = sc.nextLine().toLowerCase();
+
+        do { 
+            //two number input
+            operationPrompt();
+            System.out.print("Enter first number: ");
+            double x = sc.nextDouble();
+            System.out.print("Enter second number: ");
+            double y = sc.nextDouble();
+            sc.nextLine();
+            System.out.println();
+
+            //call operation methods
+            switch (operationChoice) {
+                case "add":
+                    double a = operations.add(x, y);
+                    return a;
+                case "subtract":
+                    double s = operations.subtract(x, y);
+                    return s;
+                case "multiply":
+                    double m = operations.multiply(x, y);
+                    return m;
+                case "divide":
+                    double d = operations.divide(x, y);
+                    return d;
+                default:
+                    try {
+                        System.out.println("Your first number is " + x + " and your second number is " + y);
+                        System.out.println("Make sure to enter the correct inputs");
+                    } catch (Exception e) {
+                        // TODO: handle exception
+                        throw new ArithmeticException("Something went wrong, internal error");
+                    }
+                    finally{
+                        System.out.println("try catch executed");
+                    }
+            }
+        } while (true);
+        /* for some unknown reason this method generates an error when the functions
+         * are put outside the do-while loop. Return keywords cannot be seen a return
+         * values - only the gods know why does that happen. DON'T PUT THE FUNCTIONS 
+         * OUT OF THE DO - WHILE LOOP.
+         */
+    }
+
+    //minor static methods
+    static void startingPrompts(){
+        System.out.println("Welcome to calculator");
+        System.out.println("Please select an operation");
+        System.out.println("----------------------------");
+        System.out.println("Add \nSubtract \nMultiply \nDivide");
+    }
+
+    static void operationPrompt(){
+        System.out.println();
+        System.out.println("Please enter two numbers");
+    }
+}
